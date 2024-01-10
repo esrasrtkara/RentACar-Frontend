63e2fc1c447159e1b8ecd47b7711d84f816a9c9f
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2024-01-10 16:29:37 +0300

    create navbar

--- rent-a-car/src/App.tsx
+++ rent-a-car/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+      </Routes>
+   
     </div>
   );
 }
---
+++ rent-a-car/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const HomePage = (props: Props) => {
+  return (
+    <div>
+        HomePage
+    </div>
+  )
+}
+
+export default HomePage
