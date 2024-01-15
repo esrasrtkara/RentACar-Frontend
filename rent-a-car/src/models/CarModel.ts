392cca0b53573edaefd520c2fb8a05a946b89352
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2024-01-15 15:31:48 +0300

    backend frontende bağlandı.

---
+++ rent-a-car/src/models/CarModel.ts
@@ -0,0 +1,12 @@
+export interface CarModel {
+	kilometer: number;
+	year: number;
+	plate: string;
+	dailyPrice: number;
+	minFindeksRate: number;
+	imagePath: string;
+	trunkVolume: number;
+	capacity: number;
+	modelName: string;
+	colorName: string;
+}
