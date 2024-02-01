1cca2ffc604773d1cde64ec1e3ab0f466acebfa8
Abdulkadir Aydemir <aydemir_0225@hotmail.com>
2024-02-01 18:13:30 +0300

    comment validation

--- rent-a-car/src/components/comment/Comments.tsx
+++ rent-a-car/src/components/comment/Comments.tsx
@@ -25,20 +25,43 @@ const Comments = (props: Props) => {
     text: " ",
   });
 
+  const [likedComments, setLikedComments] = useState<number[]>([]);
+
+  const [titleError, setTitleError] = useState<boolean>(false);
+  const [textError, setTextError] = useState<boolean>(false);
+
   const handleCommentSubmit = () => {
-    // Yeni yorumu mevcut yorumlar listesine ekleyin
-    setComments([...comments, newComment]);
-    // Yeni yorumu temizleyin
-    setNewComment({ id: newComment.id + 1, title: "", text: "" });
+    if (newComment.title.trim() === "") {
+      setTitleError(true);
+    } else {
+      setTitleError(false);
+    }
+
+    if (newComment.text.trim() === "") {
+      setTextError(true);
+    } else {
+      setTextError(false);
+    }
+
+    // Başlık ve metin doluysa yorumu ekleyin
+    if (newComment.title.trim() !== "" && newComment.text.trim() !== "") {
+      setComments([...comments, newComment]);
+      setNewComment({ id: newComment.id + 1, title: "", text: "" });
+    }
   };
 
   const handleLikeComment = (commentId: number) => {
-    // Beğeni işlemleri burada gerçekleştirilebilir
+    // Beğeni işlemleri
     console.log(`Yorum ${commentId} beğenildi.`);
+    if (likedComments.includes(commentId)) {
+      setLikedComments(likedComments.filter((id) => id !== commentId));
+    } else {
+      setLikedComments([...likedComments, commentId]);
+    }
   };
 
   const handleDeleteComment = (commentId: number) => {
-    // Silme işlemleri burada gerçekleştirilebilir
+    // Silme işlemleri
     const updatedComments = comments.filter(
       (comment) => comment.id !== commentId
     );
@@ -51,27 +74,50 @@ const Comments = (props: Props) => {
       <MDBContainer fluid className="p-3 my-5">
         {/* Yorum Yap Alanı */}
         <MDBRow>
-          <MDBCol md="6">
+          <div className="comment-header text-center mt-4">
+            <h1 className="card-title  font-weight-bold">Yorumlar</h1>
+          </div>
+          <MDBCol md="6" lg="5" xl="5" className="comment-title">
             <MDBInput
               type="text"
               label="Yorum Başlığı"
               value={newComment.title}
-              onChange={(e) =>
-                setNewComment({ ...newComment, title: e.target.value })
-              }
+              onChange={(e) => {
+                setNewComment({ ...newComment, title: e.target.value });
+                setTitleError(false); // Input değiştiğinde hatayı temizleme
+              }}
+              // Hata durumuna göre border rengini ayarlama
+              className={titleError ? "border border-danger" : ""}
             />
+            {/* Başlık hatası durumunda uyarı gösterme */}
+            {titleError && (
+              <div className="text-danger d-flex align-items-center mt-1">
+                <MDBIcon icon="exclamation-circle" className="mr-1" />
+                Lütfen bir başlık girin.
+              </div>
+            )}
           </MDBCol>
-          <MDBCol md="6">
+          <MDBCol md="6" lg="5" xl="5">
             <MDBInput
               type="textarea"
               label="Yorum Metni"
               value={newComment.text}
-              onChange={(e) =>
-                setNewComment({ ...newComment, text: e.target.value })
-              }
+              onChange={(e) => {
+                setNewComment({ ...newComment, text: e.target.value });
+                setTextError(false); // Input değiştiğinde hatayı temizleme
+              }}
+              // Hata durumuna göre border rengini ayarlama
+              className={textError ? "border border-danger" : ""}
             />
+            {/* Metin hatası durumunda uyarı gösterme */}
+            {textError && (
+              <div className="text-danger d-flex align-items-center mt-2">
+                <MDBIcon icon="exclamation-circle" className="mr-1" />
+                Lütfen bir metin girin.
+              </div>
+            )}
           </MDBCol>
-          <MDBCol md="12" className="text-center">
+          <MDBCol md="6" lg="5" xl="5" className="comment-button text-center">
             <MDBBtn color="primary" onClick={handleCommentSubmit}>
               Yorum Yap
             </MDBBtn>
@@ -81,23 +127,32 @@ const Comments = (props: Props) => {
         {/* Yapılan Yorumlar */}
         {comments.map((comment) => (
           <MDBRow key={comment.id} className="my-4 align-items-center">
-            <MDBCol md="2">
+            <MDBCol md="6" lg="5" xl="1" className="comment-img">
               {/* Kullanıcı Profil Fotoğrafı */}
               <img
                 className="img-fluid rounded-circle"
-                src="https://placekitten.com/100/100"
+                src="https://avatars.githubusercontent.com/u/56135111?v=4"
                 alt="Profil"
               />
             </MDBCol>
-            <MDBCol md="8">
+            <MDBCol md="6" lg="5" xl="3" className="comment-text">
               {/* Yorum Bilgileri */}
               <h5>{comment.title}</h5>
               <p>{comment.text}</p>
             </MDBCol>
-            <MDBCol md="2" className="text-right">
+            <MDBCol
+              md="6"
+              lg="5"
+              xl="3"
+              className="like-delete-button text-right"
+            >
               {/* Beğen ve Sil Butonları */}
               <MDBBtn
-                color="primary"
+                className="like-button"
+                outline={!likedComments.includes(comment.id)}
+                color={
+                  likedComments.includes(comment.id) ? "primary" : "secondary"
+                }
                 onClick={() => handleLikeComment(comment.id)}
               >
                 <MDBIcon icon="heart" />
