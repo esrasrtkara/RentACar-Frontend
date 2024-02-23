95e22f6aae3e786c6684f4c41242895e5ec8fdbd
Abdulkadir Aydemir <aydemir_0225@hotmail.com>
2024-02-23 20:06:51 +0300

    Order.tsx&css && components update

--- rent-a-car/src/components/carDetail/CarDetail.tsx
+++ rent-a-car/src/components/carDetail/CarDetail.tsx
@@ -1,47 +1,41 @@
-import { useEffect, useState } from 'react';
 import {
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-  MDBIcon,
   MDBBtn,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBRow,
 } from 'mdb-react-ui-kit';
-import './carDetail.css';
-import Comments from '../comment/Comments';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
 import { GetByIdCarResponse } from '../../models/responses/Car/getByIdCarResponse';
 import carService from '../../services/carService';
+import Comments from '../comment/Comments';
 import Layout from '../layout/Layout';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { GetByIdCommentResponse } from '../../models/responses/Comment/getByIdCommentResponse';
-
+import './carDetail.css';
 
 const CarDetail = () => {
   const [car, setCar] = useState<GetByIdCarResponse>();
- 
+
   const comments = useSelector((state: any) => state.comments.comments);
   const userId = useSelector((state: any) => state.userId.userId);
   let { id } = useParams();
   const name = useSelector((state: any) => state.name.name);
   const [currentUserInitial, setCurrentUserInitial] = useState<string>('');
 
-  
-
   const getCurrentUserAvatarUrl = (userId: number) => {
     const avatarUrl = `https://ui-avatars.com/api/?name=${currentUserInitial}&size=128&color=E44A48&background=FFFFFF&bold=true&font-size=0.7`;
     return avatarUrl;
   };
-  
+
   useEffect(() => {
     if (id) {
       getByIdCars();
     }
-    if(name){
-    const firstInitial = name.charAt(0).toUpperCase();
-    setCurrentUserInitial(firstInitial);
+    if (name) {
+      const firstInitial = name.charAt(0).toUpperCase();
+      setCurrentUserInitial(firstInitial);
     }
-   
   }, [name]);
 
   const getByIdCars = () => {
@@ -50,20 +44,18 @@ const CarDetail = () => {
     });
   };
 
-
   const handleDeleteComment = (commentId: number) => {
-   /* const updatedComments = comments.filter(
+    /* const updatedComments = comments.filter(
       (comment:AddCommentRequest) => comment.id !== commentId
     );*/
-   // setComments(updatedComments);
+    // setComments(updatedComments);
   };
-  console.log(comments)
+  console.log(comments);
   const navigate = useNavigate();
 
   const handleButtonClick = () => {
     navigate('/rental/' + id);
   };
-
 
   return (
     <Layout>
@@ -132,7 +124,7 @@ const CarDetail = () => {
                 <div className="col-md-4">
                   <p>
                     <MDBIcon icon="cogs" /> <strong>Vites Türü:</strong>{' '}
-                    Otomatik
+                    {car?.gearType}
                   </p>
                 </div>
               </div>
@@ -141,7 +133,7 @@ const CarDetail = () => {
                 <div className="col-md-4">
                   <p>
                     <MDBIcon icon="gas-pump" /> <strong>Yakıt Tipi:</strong>
-                    Benzinli
+                    {car?.fuelType}
                   </p>
                 </div>
                 <div className="col-md-4">
@@ -170,38 +162,45 @@ const CarDetail = () => {
         <Comments carId={Number(id)} />
 
         {comments.length !== 0 ? (
-        comments.map((comment:any,index:any) => (
-        <MDBRow key={index} className="comment my-4 align-items-center bg-border">
-            <MDBCol md="6" lg="5" xl="1" className="comment-img">
+          comments.map((comment: any, index: any) => (
+            <MDBRow
+              key={index}
+              className="comment my-4 align-items-center bg-border">
+              <MDBCol md="6" lg="5" xl="1" className="comment-img">
                 {/* Kullanıcı Profil Fotoğrafı */}
                 <img
-                    className="profile-photo img-fluid rounded-circle"
-                    src={getCurrentUserAvatarUrl(comment.userId)}
-                    alt="Profil"
-                    sizes='big'
+                  className="profile-photo img-fluid rounded-circle"
+                  src={getCurrentUserAvatarUrl(comment.userId)}
+                  alt="Profil"
+                  sizes="big"
                 />
-            </MDBCol>
-            <MDBCol md="6" lg="5" xl="3" className="comment-text">
+              </MDBCol>
+              <MDBCol md="6" lg="5" xl="3" className="comment-text">
                 {/* Yorum Bilgileri */}
                 {/*<h5>{comment.title}</h5>*/}
                 <p>{comment.text}</p>
-            </MDBCol>
-            <MDBCol md="6" lg="5" xl="3" className="like-delete-button text-right comment-container">
+              </MDBCol>
+              <MDBCol
+                md="6"
+                lg="5"
+                xl="3"
+                className="like-delete-button text-right comment-container">
                 {/* Beğen ve Sil Butonları */}
                 {comment.userId === userId ? (
-                    <MDBBtn
-                        color="danger"
-                        onClick={() => handleDeleteComment(comment.id)}
-                    >
-                        <MDBIcon icon="trash-alt" />
-                    </MDBBtn>
-                ):(<div></div>)}
-            </MDBCol>
-        </MDBRow>
-    ))
-) : (
-    <div className="no-data">No Comment</div>
-)}
+                  <MDBBtn
+                    color="danger"
+                    onClick={() => handleDeleteComment(comment.id)}>
+                    <MDBIcon icon="trash-alt" />
+                  </MDBBtn>
+                ) : (
+                  <div></div>
+                )}
+              </MDBCol>
+            </MDBRow>
+          ))
+        ) : (
+          <div className="no-data">No Comment</div>
+        )}
       </MDBContainer>
     </Layout>
   );
--- rent-a-car/src/pages/order/Order.tsx
+++ rent-a-car/src/pages/order/Order.tsx
@@ -1,28 +1,27 @@
-import { useEffect, useState } from 'react';
-import './order.css';
 import { MDBBtn } from 'mdb-react-ui-kit';
-import { GetAllRentalResponse } from '../../models/responses/Rental/getAllRentalResponse';
-import { GetAllCarResponse } from '../../models/responses/Car/getAllCarResponse';
-import invoiceService from '../../services/invoiceService';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { GetByIdInvoiceResponse } from '../../models/responses/Invoice/getByIdInvoiceResponse';
-import { useDispatch, useSelector } from 'react-redux';
+import invoiceService from '../../services/invoiceService';
+import './order.css';
+import Layout from '../../components/layout/Layout';
+import ReactDatePicker from 'react-datepicker';
 
-type Props = { 
- 
-};
+type Props = {};
 
 const Order = (props: Props) => {
-  const [invoice, setInvoice] = useState<GetByIdInvoiceResponse>()
+  const [invoice, setInvoice] = useState<GetByIdInvoiceResponse>();
   const rental = useSelector((state: any) => state.rental.rental);
+  const [returnDate, setReturnDate] = useState<Date | null>(null);
+
   useEffect(() => {
-    if(rental){
+    if (rental) {
       handleShowInvoice();
     }
-   
-   console.log(rental.id)
-    
-  }, [])
-  
+
+    console.log(rental.id);
+  }, []);
+
   const handleCancelOrder = () => {
     // Siparişi iptal etme işlemleri burada gerçekleştirilebilir
     console.log('Sipariş iptal edildi.');
@@ -34,62 +33,73 @@ const Order = (props: Props) => {
   };
 
   const handleShowInvoice = () => {
-    invoiceService.getInvoice(rental.id).then(response=>{
-      setInvoice(response.data)
-      console.log(response.data)
-    })
+    invoiceService.getInvoice(rental.id).then((response) => {
+      setInvoice(response.data);
+      console.log(response.data);
+    });
     console.log('Fatura gösterildi.');
   };
 
   return (
-    <div className="order-container">
-      <div className="order-card">
-        <div className="order-header">
-          <h1>Sipariş Detayları</h1>
-        </div>
-        <div className="order-details">
-          <div className="order-info">
-            <p>
-              <span className="bold">Sipariş No:</span>{rental.id+"12345"}
-            </p>
-            <p>
-              <span className="bold">Tarih:</span>{rental.createDate.toLocaleString()}
-            </p>
+    <Layout>
+      <div className="order-container">
+        <div className="order-card">
+          <div className="order-header">
+            <h1>Sipariş Detayları</h1>
+          </div>
+          <div className="order-details">
+            <div className="order-info">
+              <p>
+                <span className="bold">Sipariş No :</span>
+                {rental.id + '12345'}
+              </p>
+              {rental && rental.createDate && (
+                <p>
+                  <span className="bold">Tarih:</span>
+                  {rental.createDate.toLocaleString()}
+                </p>
+              )}
+            </div>
+          </div>
+          <table className="order-table">
+            <thead>
+              <tr>
+                <th>Kiralama Başlangıç Tarihi</th>
+                <th>Kiralama Bitiş Tarihi</th>
+                <th>Kiralama Dönüş Tarihi</th>
+                <th>Toplam Tutar (TL)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{rental.startDate}</td>
+                <td>{rental.endDate}</td>
+                <td className="return-date-td">
+                  <ReactDatePicker
+                    className="date"
+                    selected={returnDate}
+                    minDate={new Date(rental.endDate)}
+                    onChange={(date: Date | null) => setReturnDate(date)}
+                  />
+                </td>
+                <td>{invoice?.totalPrice}</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="order-buttons">
+            <MDBBtn className="cancel-order-button" onClick={handleCancelOrder}>
+              Sİparİşİ İptal Et
+            </MDBBtn>
+            <MDBBtn className="update-order-button" onClick={handleUpdateOrder}>
+              Sİparİşİ Güncelle
+            </MDBBtn>
+            <MDBBtn className="show-invoice-button" onClick={handleShowInvoice}>
+              Faturayı Göster
+            </MDBBtn>
           </div>
         </div>
-        <table className="order-table">
-          <thead>
-            <tr>
-              <th>Kiralama Başlangıç Tarihi</th>
-              <th>Kiralama Bitiş Tarihi</th>
-              <th>Kiralama Dönüş Tarihi</th>
-              <th>Toplam Tutar (TL)</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{rental.startDate}</td>
-              <td>{rental.endDate}</td>
-              <td>{rental.returnDate}</td>
-              <td>{invoice?.totalPrice}</td>
-            </tr>
-          </tbody>
-        </table>
-        <div className="order-buttons">
-          <MDBBtn className="cancel-order-button" onClick={handleCancelOrder}>
-            Siparişi İptal Et
-          </MDBBtn>
-          <MDBBtn className="update-order-button" onClick={handleUpdateOrder}>
-            Siparişi Güncelle
-          </MDBBtn>
-        </div>
       </div>
-      <div className="invoice-button-container">
-        <MDBBtn className="show-invoice-button" onClick={handleShowInvoice}>
-          Faturayı Göster
-        </MDBBtn>
-      </div>
-    </div>
+    </Layout>
   );
 };
 
